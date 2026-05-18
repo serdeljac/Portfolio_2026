5ff8a5e0d449ccdc8a31e9ca82ffceb1529653cf
Stjepan (Laptop) <hi@stjepanerdeljac.com>
2026-05-17 20:48:37 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header >
+      <h1>Hello World!</h1>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Root Component'
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "/src/style/variables.scss" as *;`,
+      },
+    },
+  },
+})
